41310abed02a183f20f11d019ae51ca9b11d7133
Krissada Singhakachain <46844213+OmsinKrissada@users.noreply.github.com>
2023-10-16 06:43:24 +0700

    call YouTube's API for title and thumbnail instead of mine

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,5 +1,5 @@
 import type { Config } from '@sveltejs/adapter-vercel';
-import { BYPASS_TOKEN } from '$env/static/private';
+import { BYPASS_TOKEN, YOUTUBE_API_KEY } from '$env/static/private';
 import { typeFromUrl } from '$lib/helper.js';
 
 import globeIcon from '$lib/assets/globe-icon.svg';
@@ -21,22 +21,36 @@ export async function load({ fetch }) {
 
 	const topicPromises = topicList.map(async ([topic, items]) => {
 		const itemPromises = items.map(async (item) => {
-			if (typeFromUrl(item.url) === 'article') {
+			if (typeFromUrl(item.url) === 'playlist') {
+				// const res = await fetch(`https://krissada.com/getmetatags.php?url=${item.url}`);
+				// const metaTags = await res.json();
+				const res = await fetch(`https://www.googleapis.com/youtube/v3/playlists?part=snippet,contentDetails&id=${item.url.split('=')[1]}&key=${YOUTUBE_API_KEY}`);
+				const data = await res.json();
+				return {
+					...item,
+					title: data.items[0].snippet.title,
+					// title: metaTags['og:title'],
+					thumbnailUrl: data.items[0].snippet.thumbnails.high?.url || data.items[0].snippet.thumbnails.medium?.url,
+					// thumbnailUrl: metaTags['og:image'],
+					noShadow: false
+				};
+
+			} else if (typeFromUrl(item.url) === 'video') {
+				const res = await fetch(`https://www.googleapis.com/youtube/v3/videos?part=snippet,contentDetails&id=${item.url.split('=')[1]}&key=${YOUTUBE_API_KEY}`);
+				const data = await res.json();
+				return {
+					...item,
+					// title: 'Title',
+					title: data.items[0].snippet.title,
+					thumbnailUrl: `https://i.ytimg.com/vi/${item.url.split('=')[1]}/hqdefault.jpg`,
+					noShadow: false
+				};
+			} else if (typeFromUrl(item.url) === 'article') {
 				return {
 					...item,
 					title: item.title,
 					thumbnailUrl: item.thumbnailUrl || globeIcon,
 					noShadow: !item.thumbnailUrl
-				};
-			} else {
-				const res = await fetch(`https://krissada.com/getmetatags.php?url=${item.url}`);
-				const metaTags = await res.json();
-				return {
-					...item,
-					title: metaTags['og:title'],
-					// thumbnailUrl: `https://i.ytimg.com/vi/${item.url.split('=')[1]}/maxresdefault.jpg`
-					thumbnailUrl: metaTags['og:image'],
-					noShadow: false
 				};
 			}
 		});
